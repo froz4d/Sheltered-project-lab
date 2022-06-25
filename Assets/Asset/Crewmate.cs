6a72a4a44cf079ee994a070c211f88705fa66d92
Sarabun <tuaa1bb2@gmail.com>
2022-06-25 13:42:10 +0700

    Basic Crewmate Script (HP, Hunger /w time, Work Status)

--- Assets/Asset/Crewmate.cs
+++ Assets/Asset/Crewmate.cs
@@ -1,18 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Crewmate : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int maxHp;
+    [SerializeField] private int maxHunger;
+    [SerializeField] private WorkType status;
+    
+    private int hp;
+    private float hunger;
+    
+    public enum WorkType
+    {
+        idle,
+        working,
+        hungry,
+        thirsty
+    }
+    
+    // - Unity Execute Method -
+    
     void Start()
     {
-        
+        hp = maxHp;
+        hunger = maxHunger;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Living();
+    }
+    
+    // - Custom Method -
+
+    private void Living()
+    {
+        hunger -= Time.deltaTime;
+    }
+    
+    // - Public Method -
+
+    public void SetStatus(WorkType type)
+    {
+        status = type;
     }
 }
