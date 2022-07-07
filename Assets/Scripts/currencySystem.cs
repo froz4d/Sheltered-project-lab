2fe7c49241fd4154606e5a6e6842004afd593a18
Jirateepatom <Jirateep.tana@bumail.net>
2022-07-08 00:58:12 +0700

    SciptsCoincountandCurrency
    restore scipts

---
+++ Assets/Scripts/currencySystem.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class CurrencySystem : MonoBehaviour
+{
+    public TextMeshPro txt_Currency;
+    public int defaultCurrency;
+    public int currency;
+
+    public void Init()
+    {
+        currency = defaultCurrency;
+        UpdateUI();
+    }
+
+    public void Gain(int val)
+    {
+        currency += val;
+        UpdateUI();
+    }
+
+    public bool Use(int val)
+    {
+        if (EnoughCurrency(val))
+        {
+            currency -= val;
+            UpdateUI();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool EnoughCurrency(int val)
+    {
+        if (val <= currency)
+            return true;
+        else
+            return false;
+      
+    }
+    void UpdateUI()
+    {
+        txt_Currency.text = currency.ToString();
+    }
+
+    public void USE_TEST()
+    {
+        Debug.Log(Use(3));
+    }
+
+}
