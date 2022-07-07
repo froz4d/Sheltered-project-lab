3e0fbe632d83f78f501f379a1cfd48ba9645dfc0
Jirateepatom <Jirateep.tana@bumail.net>
2022-07-07 18:01:06 +0700

    nothing
    just pushing

---
+++ Assets/Scripts/PlacementScripts.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlacementScripts : MonoBehaviour
+{
+
+    private int selectedObjectInArray;
+    private GameObject currentlySelectedObject;
+
+    //set array
+    [SerializeField] private GameObject[] selectableObjects;
+    
+    //automatically set to false
+    private bool isAnObjectSelected = false;
+    
+
+    
+
+    // Start selecting none object
+    void Start()
+    {
+        selectedObjectInArray = 0;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        //Make it placetable and stick to the Grid By round it
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 spawnPos = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+        
+        //If select E key it will select your object เข้าสู่โหมดการส้ราง
+        if (Input.GetKeyDown("e") && isAnObjectSelected == false)
+        {
+            //If press E key select Object
+            currentlySelectedObject = Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+            isAnObjectSelected = true;
+            
+            
+        }
+
+        
+        //Unselect the object from E key by "Right click"
+        if (Input.GetMouseButtonDown(1) && isAnObjectSelected == true)
+        {
+            Destroy(currentlySelectedObject);
+            isAnObjectSelected = false;
+            selectedObjectInArray = 0;
+        }
+        
+        
+        //Mouse Scroll Wheel to pick array >0 
+        if (Input.GetAxis("Mouse ScrollWheel") > 0 && isAnObjectSelected == true)
+        {
+            selectedObjectInArray++;
+                
+            if (selectedObjectInArray > selectableObjects.Length - 1)
+            {
+                selectedObjectInArray = 0;
+            }
+            
+            //change the item that we select to the next one
+            Destroy(currentlySelectedObject);
+            currentlySelectedObject = Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+            
+        }
+        
+        //Mouse Scroll Wheel to pick array <0
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0 && isAnObjectSelected == true)
+        {
+            selectedObjectInArray--;
+            
+            if (selectedObjectInArray < 0)
+            {
+                selectedObjectInArray = selectableObjects.Length - 1;
+            }
+            //change the item that we select to the next one
+            Destroy(currentlySelectedObject);
+            currentlySelectedObject = Instantiate(selectableObjects[selectedObjectInArray], spawnPos, Quaternion.identity);
+            
+        }
+        //text.text = "" + count;
+        
+    }
+
+    
+}       
+    
---
+++ Assets/Scripts/objectPlacement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objectPlacement : MonoBehaviour
+{
+    //costumize the object final build
+    [SerializeField] private GameObject placeObject;
+    [SerializeField] private LayerMask allTilesLayer;
+    [SerializeField] private GameObject bluePrint;
+
+
+    //variable
+    private Vector2 mousePos;
+
+
+
+    void Update()
+    {
+        //stick selected object to the mouse 
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(Mathf.Round(mousePos.x), Mathf.Round(mousePos.y));
+
+
+        //Place the Object 
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            //raycast from camera infinite
+            RaycastHit2D rayHit = Physics2D.Raycast(transform.position, Vector2.zero, Mathf.Infinity, allTilesLayer);
+
+
+
+            // Update is called once per frame
+            if (rayHit.collider == null && gameObject.CompareTag("objectTag"))
+            {
+                GameObject placedObject = Instantiate(bluePrint, transform.position, Quaternion.identity);
+                //Debug.Log("You did not place object");
+                Vector2 afterDelayPos = placedObject.transform.position;
+                
+                StartCoroutine(DelayConstruction(afterDelayPos, placedObject));
+            }
+            if (rayHit.collider == null && gameObject.CompareTag("platformTag"))
+            {
+                GameObject placedObject = Instantiate(bluePrint, transform.position, Quaternion.identity);
+                //Debug.Log("You did not place object");
+                Vector2 afterDelayPos = placedObject.transform.position;
+                
+                StartCoroutine(DelayConstruction(afterDelayPos, placedObject));
+            }
+
+        }
+    }
+    IEnumerator DelayConstruction(Vector2 position, GameObject go)
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(go);
+        Instantiate(placeObject, position, Quaternion.identity);
+    }
+    //
+}
