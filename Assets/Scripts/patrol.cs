952d83c648664441d2f9cb313671cad7c11da659
Jirateepatom <Jirateep.tana@bumail.net>
2022-07-13 15:28:36 +0700

    Ai Pratrol
    Ai pratrol Random Place In Scene

---
+++ Assets/Scripts/patrol.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+
+
+public class patrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    
+
+    public Transform moveSpots;
+    public float minX;
+    public float minY;
+    public float maxX;
+    public float maxY;
+
+
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+
+        moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+
+    }
+
+    //move character to random spot
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots.position, speed * Time.deltaTime);
+        
+        //wait for the character to travel to the random location first
+        if(Vector2.Distance(transform.position, moveSpots.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                waitTime = startWaitTime;
+                moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
